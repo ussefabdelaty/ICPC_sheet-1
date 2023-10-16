aa3f78b63dce4645e4733a080ac1d5b9b74870df
unknown <ussifyounis@gmail.com>
2023-10-16 10:32:02 +0300

    the solution of problem D

---
+++ D_Difference/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Difference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputs = Console.ReadLine();
+
+            string[] inputs_numbers = inputs.Split(' ');
+
+
+            if (inputs_numbers.Length == 4 &&
+                long.TryParse(inputs_numbers[0], out long A)&&
+                long.TryParse(inputs_numbers[1], out long B)&&
+                long.TryParse(inputs_numbers[2], out long C)&&
+                long.TryParse(inputs_numbers[3], out long D))
+            {
+                if (A >= -100000 && A <= 100000 &&
+                    B >= -100000 && B <= 100000 &&
+                    C >= -100000 && C <= 100000 &&
+                    D >= -100000 && D <= 100000)
+                {
+                    long X = (A * B) - (C * D);
+                    Console.WriteLine($"Difference = {X}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Inputs");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid Inputs");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ D_Difference/ReadMe.txt
@@ -0,0 +1,11 @@
+D. Difference
+
+Given four numbers A, B, C and D. Print the result of the following equation :
+
+ X = (A * B) - (C * D).
+
+Input
+Only one line containing 4 separated numbers A, B, C and D ( - 105  ≤  A, B, C, D  ≤  105).
+
+Output
+Print "Difference  =  " without quotes followed by the equation result.
