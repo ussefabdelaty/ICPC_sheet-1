2baff4adc9194d642c90eee4e0d347c757e6c689
unknown <ussifyounis@gmail.com>
2023-10-16 10:31:17 +0300

    the solution of problem C

---
+++ C_Simple_calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Simple_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string GetNumbers = Console.ReadLine();
+
+            string[] GetNum = GetNumbers.Split();
+
+           if (GetNum.Length ==2 && long.TryParse(GetNum[0] , out long x ) 
+                && long.TryParse(GetNum[1],out long y))
+            {
+                Console.WriteLine($"{x} + {y} = {x+y}");
+                Console.WriteLine($"{x} * {y} = {x*y}");
+                Console.WriteLine($"{x} - {y} = {x-y}");
+            }
+           else
+            {
+                Console.WriteLine("unvalid inputs ,try again!");
+            }
+
+
+
+
+
+        }
+    }
+}
---
+++ C_Simple_calculator/ReadMe.txt
@@ -0,0 +1,13 @@
+C. Simple Calculator
+
+Given two numbers X and Y. Print the summation and multiplication and subtraction of these 2 numbers.
+
+Input
+Only one line containing two separated numbers X, Y (1  ≤  X, Y  ≤  105).
+
+Output
+Print 3 lines that contain the following in the same order:
+
+"X + Y = summation result" without quotes.
+"X * Y = multiplication result" without quotes.
+"X - Y = subtraction result" without quotes.
