3b95bfb8d5b2f0dc38f89130ca1c3812421f7fe2
unknown <ussifyounis@gmail.com>
2023-10-16 10:30:10 +0300

    the solution of the problem B in sheet#1

---
+++ Basic_DT/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Basic_DT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Only one line containing the following space-separated values: int, long long, char, float and double respectively.");
+          string Integers =   Console.ReadLine();
+            string[] arrnumbers = Integers.Split();
+
+            if (arrnumbers.Length == 5 &&
+                int.TryParse(arrnumbers[0], out int x ) &&
+                long.TryParse(arrnumbers[1],    out long y ) && 
+                char.TryParse(arrnumbers[2],    out char c ) &&
+                float.TryParse(arrnumbers[3],   out float f ) &&
+                double.TryParse(arrnumbers[4],  out double d ) 
+                )
+            {
+                Console.WriteLine(x);
+                Console.WriteLine(y);
+                Console.WriteLine(c);
+                Console.WriteLine(f);
+               // Console.WriteLine(d);
+                string formattedNumber = d.ToString("0.0");
+                Console.WriteLine(formattedNumber); ;
+            }
+            else
+            {
+                Console.WriteLine("enter valid inputs!");
+            }
+        }
+    }
+}
---
+++ Basic_DT/ReadMe.txt
@@ -0,0 +1,36 @@
+B. Basic Data Types
+
+int : 32 Bit integer.
+long long : 64 bit integer
+Char : 8 bit Characters & symbols
+Float : 32 bit real value
+Double : 64 bit real value
+Reading
+
+To read a data type, use the following syntax:
+
+   cin >> VariableName;
+ 
+For example, to read a character followed by a double:
+  char ch;
+  double d;
+  cin >> ch >> d;
+ 
+Printing
+
+To print a data type, use the following syntax:
+
+cout << VariableName;
+ 
+For example, to print a character followed by a double:
+char ch = 'd';
+double d = 234.432;
+cout << ch << " "<< d;
+ 
+Input
+Only one line containing the following space-separated values: int, long long, char, float and double respectively.
+
+Output
+Print each element on a new line in the same order it was received as input.
+
+Don't print any extra spaces.
