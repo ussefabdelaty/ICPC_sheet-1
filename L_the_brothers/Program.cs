f0c916b1431da978df610b6100cf6a1778ee8dc2
unknown <ussifyounis@gmail.com>
2023-10-17 18:32:59 +0300

    the problem L

---
+++ L_the_brothers/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace L_the_brothers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          /*  string FirstPerson = Console.ReadLine();
+            string[] Fperson = FirstPerson.Split(' ');
+
+            string SecondPerson  = Console.ReadLine(); 
+            string[] Sperson = SecondPerson.Split(' ');
+
+
+            if (Fperson.Length == SecondPerson.Length | Fperson[1] == Sperson[1])
+            {
+                Console.WriteLine("ARE Brothers");
+            }
+            else
+            {
+                Console.WriteLine("NOT");
+            }*/
+
+            //another solution 
+
+            Console.WriteLine("Enter the first person's first and second name:");
+            string[] person1Names = Console.ReadLine().Split(' ');
+
+            Console.WriteLine("Enter the second person's first and second name:");
+            string[] person2Names = Console.ReadLine().Split(' ');
+
+            string secondName1 = person1Names.Length > 1 ? person1Names[1] : "";
+            string secondName2 = person2Names.Length > 1 ? person2Names[1] : "";
+
+            if (secondName1 == secondName2)
+            {
+                Console.WriteLine("ARE Brothers");
+            }
+            else
+            {
+                Console.WriteLine("NOT");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ L_the_brothers/ReadMe.txt
@@ -0,0 +1,17 @@
+L. The Brothers
+
+Given two person names.
+
+Each person has {"the first name" + "the second name"}
+
+Determine whether they are brothers or not.
+
+Note: The two persons are brothers if they share the same second name.
+
+Input
+First line will contain two Strings F1, S1 which donates the first and second name of the 1st person.
+
+Second line will contain two Strings F2, S2 which donates the first and second name of the 2nd person.
+
+Output
+Print "ARE Brothers" if they are brothers otherwise print "NOT".
