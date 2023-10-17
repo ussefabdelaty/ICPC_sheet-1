5f248ee135126d85eadf164f1c3d0f38713a3524
unknown <ussifyounis@gmail.com>
2023-10-17 17:48:50 +0300

    the problem J

---
+++ J_multiplies/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace J_multiplies
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string inputs = Console.ReadLine();
+
+            string[] arr_inputs = inputs.Split(' ');
+
+            if (arr_inputs.Length == 2 &&
+                int.TryParse(arr_inputs[0],out int A)&&
+                int.TryParse(arr_inputs[1], out int B)
+                ) 
+            {
+                if (A % B == 0 || B % A == 0)
+                {
+                    Console.WriteLine("Multiples");
+                }
+                else
+                {
+                    Console.WriteLine("No Multiples");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("input valid numbers!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ J_multiplies/ReadMe.txt
@@ -0,0 +1,9 @@
+J. Multiples
+
+Given two numbers A and B. Print "Multiples" if A is multiple of B or vice versa. Otherwise print "No Multiples".
+
+Input
+Only one line containing two numbers A, B (1  ≤  A, B  ≤  106)
+
+Output
+Print the "Multiples" or "No Multiples" corresponding to the read numbers.
