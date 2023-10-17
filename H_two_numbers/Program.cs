2c2ac2332246af043153365a2cc13585a15024c7
unknown <ussifyounis@gmail.com>
2023-10-17 16:31:12 +0300

    the problem number H

---
+++ H_two_numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace H_two_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string numbers = Console.ReadLine();
+
+            string[] arrNumbers = numbers.Split(' ');
+            
+            if(arrNumbers.Length == 2 &&
+                double.TryParse(arrNumbers[0], out double A) &&
+                double.TryParse(arrNumbers[1], out double B)&& B !=0)
+            {
+                double cal = A / B;
+                Console.WriteLine($"floor {A} / {B} = {Math.Floor(cal)}");
+                Console.WriteLine($"ceil {A} / {B} = {Math.Ceiling(cal)}");
+                Console.WriteLine($"round {A} / {B} = {Math.Round(cal, MidpointRounding.AwayFromZero)}");
+            }
+            else { Console.WriteLine("enter valid numbers"); }
+
+            Console.ReadKey();  
+
+            
+        }
+    }
+}
+
+
---
+++ H_two_numbers/ReadMe.txt
@@ -0,0 +1,36 @@
+H. Two numbers
+
+Given 2 numbers A
+ and B
+. Print floor, ceil and round of A/B
+
+Note:
+
+Floor: Is a mathematical function that takes a real number X
+ and its output is the greatest integer less than or equal to X
+.
+Ceil: Is a mathematical function that takes a real number X
+ and its output is the smallest integer larger than or equal to X
+.
+Round: Is a mathematical function that takes a real number X
+ and its output is the closest integer to that number X
+.
+
+For more clarification visit the links in the notes below.
+
+Input
+Only one line containing two numbers A
+ and B
+ (1≤A,B≤103)
+Output
+Print 3 lines that contain the following in the same order:
+
+"floor A
+ / B
+ = Floor result" without quotes.
+"ceil A
+ / B
+ = Ceil result" without quotes.
+"round A
+ / B
+ = Round result" without quotes.
