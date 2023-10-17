e7c6181d92ddebe9a0f434a1edcf635aa0fec9c9
unknown <ussifyounis@gmail.com>
2023-10-17 17:48:13 +0300

    th problem I

---
+++ I_welcome_for_you/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace I_welcome_for_you
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputs = Console.ReadLine();
+            string [] arr_inputs = inputs.Split(' ');
+            if(arr_inputs.Length == 2 &&
+                int.TryParse(arr_inputs[0], out int A)&&
+                int.TryParse(arr_inputs[1], out int B) 
+                )
+            { 
+                if (A >= B)
+                {
+                    Console.WriteLine("Yes");
+                }
+                else { Console.WriteLine("No"); }
+            }
+            else
+            {
+                Console.WriteLine("enter valid inputs");
+            }
+
+        }
+    }
+}
---
+++ I_welcome_for_you/ReadMe.txt
@@ -0,0 +1,9 @@
+I. Welcome for you with Conditions
+
+Given two numbers A and B. Print "Yes" if A is greater than or equal to B. Otherwise print "No".
+
+Input
+Only one line containing two numbers A and B (0  ≤  A, B  ≤  100).
+
+Output
+Print "Yes" or "No" according to the statement.
