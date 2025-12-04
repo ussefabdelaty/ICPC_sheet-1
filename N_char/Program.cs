118b16ee1345244c1ffa1f658783a80aacc1a939
ussefabdelaty <96540998+ussefabdelaty@users.noreply.github.com>
2025-12-04 07:33:02 +0200

    Add files via upload

---
+++ N_char/Program.cs
@@ -0,0 +1,57 @@
+﻿/*namespace N_char
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            char.TryParse(Console.ReadLine(), out char x); 
+            
+            if (char.IsUpper(x))
+            {
+               
+                Console.WriteLine(char.ToLower(x));
+            }
+            else if (char.IsLower(x))
+            {
+                Console.WriteLine(char.ToUpper(x)); 
+            }
+            else
+            {
+                Console.WriteLine("enter a valid input"); 
+            }
+
+            Console.ReadKey();
+        }
+    }
+}*/
+
+//the best soltuion for runtime 
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        char x;
+        if (char.TryParse(Console.ReadLine(), out x))
+        {
+            if (char.IsLetter(x))
+            {
+                // Toggle the case
+                char convertedCharacter = char.IsUpper(x) ? char.ToLower(x) : char.ToUpper(x);
+                Console.WriteLine(convertedCharacter);
+            }
+            else
+            {
+                Console.WriteLine("Enter a valid input.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Enter a valid input.");
+        }
+
+        Console.ReadKey();
+    }
+}
